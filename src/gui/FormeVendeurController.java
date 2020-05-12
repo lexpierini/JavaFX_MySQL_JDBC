57ee8ca30b3ce428317aeda60beacbb4d676b5e7
lexpierini <alex_pierini@hotmail.com>
2020-05-12 10:02:05 -0400

    FormeVendeur

---
+++ src/gui/FormeVendeurController.java
@@ -0,0 +1,126 @@
+package gui;
+
+import db.DbException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import model.entities.Vendeur;
+import model.exceptions.ValidationException;
+import model.services.ServiceVendeur;
+
+import java.net.URL;
+import java.util.*;
+
+public class FormeVendeurController implements Initializable {
+    private Vendeur entity;
+    private ServiceVendeur service;
+    private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
+    @FXML
+    private TextField txtId;
+    @FXML
+    private TextField txtNom;
+    @FXML
+    private Label labelErreurNom;
+    @FXML
+    private Button btEnregistrer;
+    @FXML
+    private Button btAnnuler;
+
+    public void setVendeur(Vendeur entity) {
+        this.entity = entity;
+    }
+
+    public void setServiceVendeur(ServiceVendeur service) {
+        this.service = service;
+    }
+
+    public void subscribeDataChangeListener(DataChangeListener listener) {
+        dataChangeListeners.add(listener);
+    }
+
+    @FXML
+    public void onBtEnregistrerAction(ActionEvent event) {
+        if (entity == null) {
+            throw new IllegalStateException("L'entité était nulle.");
+        }
+        if (service == null) {
+            throw new IllegalStateException("Service était nul");
+        }
+        try {
+            entity = getFormData();
+            service.saveOrUpdate(entity);
+            notifyDataChangeListeners();
+            Utils.currentStage(event).close();
+        }
+        catch (ValidationException e) {
+            setErrorMessages(e.getErrors());
+        }
+        catch (DbException e) {
+            Alerts.showAlert("Erreur lors de l'enregistrement de l'objet.", null, e.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
+
+    private void notifyDataChangeListeners() {
+        for (DataChangeListener listener : dataChangeListeners) {
+            listener.onDataChanged();
+        }
+    }
+
+    private Vendeur getFormData() {
+        Vendeur obj = new Vendeur();
+
+        ValidationException exception = new ValidationException("Erreur de validation.");
+
+        obj.setId(Utils.tryParseToInt(txtId.getText()));
+
+        if (txtNom.getText() == null || txtNom.getText().trim().equals("")) {
+            exception.addError("nom", "Le champ ne peut pas être vide.");
+        }
+        obj.setNom(txtNom.getText());
+
+        if (exception.getErrors().size() > 0) {
+            throw exception;
+        }
+
+        return obj;
+    }
+
+    @FXML
+    public void onBtAnnulerAction(ActionEvent event) {
+        Utils.currentStage(event).close();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        initializeNodes();
+    }
+
+    private void initializeNodes() {
+        Constraints.setTextFieldInteger(txtId);
+        Constraints.setTextFieldMaxLength(txtNom, 30);
+    }
+
+    public void updateFormData() {
+        if (entity == null) {
+            throw new IllegalStateException("L'entité était nulle.");
+        }
+        txtId.setText(String.valueOf(entity.getId()));
+        txtNom.setText(entity.getNom());
+    }
+
+    private void setErrorMessages(Map<String, String> errors) {
+        Set<String> fields = errors.keySet();
+
+        if (fields.contains("nom")) {
+            labelErreurNom.setText(errors.get("nom"));
+        }
+    }
+}
--- src/gui/ListeVendeurController.java
+++ src/gui/ListeVendeurController.java
@@ -10,14 +10,19 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Vendeur;
 import model.services.ServiceVendeur;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Date;
 import java.util.List;
@@ -92,27 +97,27 @@ public class ListeVendeurController implements Initializable, DataChangeListener
 
     // Ouvre la fenêtre du formulaire des départements à remplir.
     private void createDialogForm(Vendeur obj, String absoluteName, Stage parentStage) {
-//        try {
-//            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-//            Pane pane = loader.load();
-//
-//            FormeVendeurController controller = loader.getController();
-//            controller.setVendeur(obj);
-//            controller.setServiceVendeur(new ServiceVendeur());
-//            controller.subscribeDataChangeListener(this);
-//            controller.updateFormData();
-//
-//            Stage dialogStage = new Stage();
-//            dialogStage.setTitle("Entrer les données du département:");
-//            dialogStage.setScene(new Scene(pane));
-//            dialogStage.setResizable(false);
-//            dialogStage.initOwner(parentStage);
-//            dialogStage.initModality(Modality.WINDOW_MODAL); // Affiche la fenêtre précédente uniquement si la fenêtre actuelle est fermée.
-//            dialogStage.showAndWait();
-//        }
-//        catch (IOException e) {
-//            Alerts.showAlert("IO Exception", "Erreur lors du chargement de la vue.", e.getMessage(), AlertType.ERROR);
-//        }
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+            Pane pane = loader.load();
+
+            FormeVendeurController controller = loader.getController();
+            controller.setVendeur(obj);
+            controller.setServiceVendeur(new ServiceVendeur());
+            controller.subscribeDataChangeListener(this);
+            controller.updateFormData();
+
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Entrer les données du vendeur:");
+            dialogStage.setScene(new Scene(pane));
+            dialogStage.setResizable(false);
+            dialogStage.initOwner(parentStage);
+            dialogStage.initModality(Modality.WINDOW_MODAL); // Affiche la fenêtre précédente uniquement si la fenêtre actuelle est fermée.
+            dialogStage.showAndWait();
+        }
+        catch (IOException e) {
+            Alerts.showAlert("IO Exception", "Erreur lors du chargement de la vue.", e.getMessage(), AlertType.ERROR);
+        }
     }
 
     @Override
